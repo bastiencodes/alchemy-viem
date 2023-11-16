e64b9bcbc2c867750e5db316a4efbac06d51b1db
Bastien <bastienm95@gmail.com>
2023-11-16 04:07:08 +0100

    Update example

--- examples/viem.ts
+++ examples/viem.ts
@@ -1,16 +1,31 @@
-import { createAlchemyClient } from "../src";
+import { Alchemy } from "../src";
+import { createAlchemyClient } from "../src/client/createAlchemyClient";
 import { Network } from "../src/client/networks";
 
-const alchemy = createAlchemyClient({
-  network: Network.ETH_MAINNET,
-  apiKey: process.env.ALCHEMY_API_KEY
+// const viemClient = createAlchemyClient({
+//   network: Network.ETH_MAINNET,
+//   apiKey: process.env.ALCHEMY_API_KEY
+// });
+
+// const address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+// const ensName = "vitalik.eth";
+
+// // TODO: add pagination
+// const tokenBalances = await viemClient.getTokenBalances({
+//   addressOrEnsName: ensName
+// });
+// console.log("Result", tokenBalances);
+
+const alchemy = new Alchemy({
+  networks: [Network.ETH_MAINNET],
+  apiKey: process.env.ALCHEMY_API_KEY as string,
 });
 
-const address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+// const address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
 const ensName = "vitalik.eth";
 
-// TODO: add pagination
-const tokenBalances = await alchemy.getTokenBalances({
-  addressOrEnsName: ensName
+// // TODO: add pagination
+const tokenBalances = await alchemy._clients[0].getTokenBalances({
+  addressOrEnsName: ensName,
 });
 console.log("Result", tokenBalances);
