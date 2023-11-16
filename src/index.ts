ab11543c728202cde076e015e5b5768319be382c
Bastien <bastienm95@gmail.com>
2023-11-16 04:06:07 +0100

    Update index.ts to export Alchemy wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export { createAlchemyClient } from "./client/createAlchemyClient";
+import {
+  AlchemyClient,
+  createAlchemyClient,
+} from "./client/createAlchemyClient";
+import { Network } from "./client/networks";
+
+type Config = {
+  apiKey: string;
+  networks: Network[];
+};
+
+export class Alchemy {
+  public _clients: AlchemyClient[];
+
+  constructor({ networks, apiKey }: Config) {
+    this._clients = initalizeClients({ networks, apiKey });
+  }
+}
+
+function initalizeClients({ networks, apiKey }: Config) {
+  return networks.map((network) => {
+    return createAlchemyClient({
+      network,
+      apiKey,
+    });
+  });
+}
